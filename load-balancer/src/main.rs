f03039a01b2cf15d2633a1cdedc304b8b5c9929c
Gaurav Kumar <gauravanurag36@gmail.com>
2026-02-23 12:57:45 +0530

    practice old commits

---
+++ load-balancer/src/main.rs
@@ -0,0 +1,5 @@
+const SERVER_ADDR: &str = "127.0.0.1:8080";
+
+fn main() {
+    println!("server is {}", SERVER_ADDR);
+}
--- structs/src/main.rs
+++ structs/src/main.rs
@@ -12,9 +12,13 @@ fn main() {
         String::from("gauravanurag"),
     );
 
-    println!("{:#?}", user1);
+    println!("{:#?}", user1.active);
+    println!("{:#?}", user1.username);
+    println!("{:#?}", user1.email);
+    println!("{:#?}", user1.sign_in_count);
 }
 
+// this function builds a new user
 fn build_user(email: String, username: String) -> User {
     User {
         active: true,
